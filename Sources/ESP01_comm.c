bfadaa8cddecdd38f14d6574547bd7e8e9a158a9
guilhermerc <guilherme.riciolic@gmail.com>
2019-03-30 15:39:49 -0300

    Details in the commit message
    1. Removed the '\"" characters from MQTT_PORT macro (it shouldn't
    have according to the protocol)
    2. Fixed a typo in MQTT_PASSWORD (it was MQTT_PSSWORD)
    3. Changing the COMM_STATE GET_MAC_ADD to GET_MAC_ADDR
    4. Encapsulating the sending message function (comm_send_msg now)
    5. Implementing the GET_IP_NUMB and GET_MAC_ADDR in the
    comm_fsm_recv function
    (Also checking if we already have the IP number and the MAC addres.
    Since they won't change, we don't need to get'em again. In other
    words, if we already have the IP number and the MAC address, jumps
    from CONNECT_WIFI to CONNECT_MQTT.)
    6. Implementing the GET_IP_NUMB, GET_MAC_ADDR and CONNECT_MQTT
    state in the comm_fsm_send functon.

--- Sources/ESP01_comm.c
+++ Sources/ESP01_comm.c
@@ -8,20 +8,27 @@
 #include <ESP01_comm.h>
 #include <LOG.h>
 #include <string.h>
+#include <UART0.h>
 #include <UART2Events.h>
 
 #define WIFI_SSID "\"EA076\""
 #define WIFI_PASSWORD "\"FRDMKL25Z\""
-#define MQTT_PORT "\"1883\""
+#define MQTT_PORT "1883"
 #define MQTT_USERNAME "\"GUILHERME\""
-#define MQTT_PSSWORD "\"UNICAMP\""
+#define MQTT_PASSWORD "\"UNICAMP\""
 #define TERMINATING_CHARS "\r\n"
+
+#define IP_NUMBER_SIZE	32
+static char ip_number[IP_NUMBER_SIZE];	// it should be const
+
+#define MAC_ADDR_SIZE	64
+static char mac_addr[MAC_ADDR_SIZE];	// it should be const
 
 typedef enum COMM_STATE
 {
 	CONNECT_WIFI,
 	GET_IP_NUMB,
-	GET_MAC_ADD,
+	GET_MAC_ADDR,
 	CONNECT_MQTT,
 	INDEED_COMM
 } COMM_STATE_ENUM;
@@ -35,8 +42,16 @@ void comm_fsm_start()
 	comm_fsm_send();
 }
 
+void comm_send_msg()
+{
+	UART2_OnTxChar();
+}
+
 void comm_fsm_recv(UART2_TComData* message_in)
 {
+	static bool has_ip_number = FALSE;
+	static bool has_mac_addr = FALSE;
+
 	switch(comm_state)
 	{
 	case CONNECT_WIFI:
@@ -45,15 +60,16 @@ void comm_fsm_recv(UART2_TComData* message_in)
 
 		if(strcmp(message_in, "CONNECT WIFI\r\n") == 0)
 		{
-			comm_state = GET_IP_NUMB;
+			if(has_ip_number != TRUE)
+				comm_state = GET_IP_NUMB;
+			else if(has_mac_addr != TRUE)
+				comm_state = GET_MAC_ADDR;
+			else
+				comm_state = CONNECT_MQTT;
 		}
 		else if(strcmp(message_in, "ERROR WIFI\r\n") == 0)
 		{
 			comm_state = CONNECT_WIFI;
-		}
-		else
-		{
-			//LOG("[UNEXPECTED] comm_fsm_recv (CONNECT_WIFI)", message_in);
 		}
 
 		break;
@@ -61,11 +77,45 @@ void comm_fsm_recv(UART2_TComData* message_in)
 	case GET_IP_NUMB:
 	{
 		LOG("comm_fsm_recv (GET_IP_NUMB)", message_in);
+		if(strcmp(message_in, "NOIP\r\n") == 0)
+		{
+			comm_state = GET_IP_NUMB;
+		}
+		else if(strcmp(message_in, "WIFI_DISCONNECTED\r\n") == 0)
+		{
+			comm_state = CONNECT_WIFI;
+		}
+		else
+		{
+			strcpy(ip_number, "\"");
+			strcat(ip_number, message_in);
+			// The index which corresponds to the '\r' char is overwritten with '\"'
+			ip_number[strlen(message_in) - 1] = '\"';
+			// The index which corresponds to the '\n' char is overwritten with '\0'
+			ip_number[strlen(message_in)] = '\0';
+			has_ip_number = TRUE;
+			comm_state = GET_MAC_ADDR;
+		}
 		break;
 	}
-	case GET_MAC_ADD:
+	case GET_MAC_ADDR:
 	{
-		LOG("comm_fsm_recv (GET_MAC_ADD)", message_in);
+		LOG("comm_fsm_recv (GET_MAC_ADDR)", message_in);
+		if(strcmp(message_in, "WIFI_DISCONNECTED\r\n") == 0)
+		{
+			comm_state = CONNECT_WIFI;
+		}
+		else
+		{
+			strcpy(mac_addr, "\"");
+			strcat(mac_addr, message_in);
+			// The index which corresponds to the '\r' char is overwritten with '\"'
+			mac_addr[strlen(message_in) - 1] = '\"';
+			// The index which corresponds to the '\n' char is overwritten with '\0'
+			mac_addr[strlen(message_in)] = '\0';
+			has_mac_addr = TRUE;
+			comm_state = CONNECT_MQTT;
+		}
 		break;
 	}
 	case CONNECT_MQTT:
@@ -94,24 +144,48 @@ void comm_fsm_send()
 		strcat(message_out, WIFI_PASSWORD);
 		strcat(message_out, TERMINATING_CHARS);
 
-		LOG("comm_fsm_send (CONNECT_WIFI)", message_out);
-		UART2_OnTxChar();
+		//LOG("comm_fsm_send (CONNECT_WIFI)", message_out);
+		comm_send_msg();
 
 		break;
 	}
 	case GET_IP_NUMB:
 	{
-		LOG("comm_fsm_send (GET_IP_NUMB)", message_out);
+		strcpy(message_out, "GETIP");
+		strcat(message_out, TERMINATING_CHARS);
+
+		//LOG("comm_fsm_send (GET_IP_NUMB)", message_out);
+		comm_send_msg();
+
 		break;
 	}
-	case GET_MAC_ADD:
+	case GET_MAC_ADDR:
 	{
-		LOG("comm_fsm_send (GET_MAC_ADD)", message_out);
+		strcpy(message_out, "GETMAC");
+		strcat(message_out, TERMINATING_CHARS);
+
+		//LOG("comm_fsm_send (GET_MAC_ADDR)", message_out);
+		comm_send_msg();
+
 		break;
 	}
 	case CONNECT_MQTT:
 	{
-		LOG("comm_fsm_send (CONNECT_MQTT)", message_out);
+		strcpy(message_out, "CONNMQTT ");
+		strcat(message_out, ip_number);
+		strcat(message_out, ",");
+		strcat(message_out, MQTT_PORT);
+		strcat(message_out, ",");
+		strcat(message_out, mac_addr);
+		strcat(message_out, ",");
+		strcat(message_out, MQTT_USERNAME);
+		strcat(message_out, ",");
+		strcat(message_out, MQTT_PASSWORD);
+		strcat(message_out, TERMINATING_CHARS);
+
+		//LOG("comm_fsm_send (CONNECT_MQTT)", message_out);
+		comm_send_msg();
+
 		break;
 	}
 	case INDEED_COMM:
