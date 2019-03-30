ddb86260339277ebc171eb3b844c99fdb529294c
guilhermerc <guilherme.riciolic@gmail.com>
2019-03-30 00:28:25 -0300

    Properly setting the macros ('"' issue) and implementing the first state of the FSM

--- Sources/ESP01_comm.c
+++ Sources/ESP01_comm.c
@@ -5,12 +5,17 @@
 **         with the MQTT Broker (using the ESP-01 module).
 */
 
+#include <string.h>
+#include <UART2.h>
+#include "ESP01_comm.h"
+#include "UART2Events.h"
 
-#define WIFI_SSID "EA076"
-#define WIFI_PASSWORD "FRDMKL25Z"
-#define MQTT_PORT "1883"
-#define MQTT_USERNAME "GUILHERME"
-#define MQTT_PSSWORD "UNICAMP"
+#define WIFI_SSID "\"EA076\""
+#define WIFI_PASSWORD "\"FRDMKL25Z\""
+#define MQTT_PORT "\"1883\""
+#define MQTT_USERNAME "\"GUILHERME\""
+#define MQTT_PSSWORD "\"UNICAMP\""
+#define TERMINATING_CHARS "\r\n"
 
 typedef enum COMM_STATE
 {
@@ -21,13 +26,63 @@ typedef enum COMM_STATE
 	INDEED_COMM
 } COMM_STATE_ENUM;
 
-COMM_STATE_ENUM comm_state = CONNECT_WIFI;
+static COMM_STATE_ENUM comm_state = CONNECT_WIFI;
 
-void communication_fsm()
+void comm_fsm_start()
+{
+	comm_state = CONNECT_WIFI;
+	comm_fsm_send();
+}
+
+void comm_fsm_parse(UART2_TComData* message)
 {
 	switch(comm_state)
 	{
 	case CONNECT_WIFI:
+	{
+		if(strcmp(message, "CONNECT WIFI\r\n") == 0)
+		{
+			comm_state = GET_IP_NUMB;
+		}
+		else if(strcmp(message, "ERROR WIFI\r\n") == 0)
+		{
+			comm_state = CONNECT_WIFI;
+		}
+		else
+		{
+			// LOG the message into UART0: "comm_fsm_parse: <message>"
+		}
+		break;
+	}
+	case GET_IP_NUMB:
+	case GET_MAC_ADD:
+	case CONNECT_MQTT:
+	case INDEED_COMM:
+	{
+		break;
+	}
+	}
+}
+
+void comm_fsm_send()
+{
+	UART2_TComData message[MESSAGE_BUFFER_SIZE];
+
+	switch(comm_state)
+	{
+	case CONNECT_WIFI:
+	{
+		strcpy(message, "CONNWIFI ");
+		strcat(message, WIFI_SSID);
+		strcat(message, ",");
+		strcat(message, WIFI_PASSWORD);
+		strcat(message, TERMINATING_CHARS);
+		/*
+		 * Call function that sends a string via UART2
+		 */
+		// LOG the message into UART0: "comm_fsm_send: <message>"
+		break;
+	}
 	case GET_IP_NUMB:
 	case GET_MAC_ADD:
 	case CONNECT_MQTT:
--- Sources/ESP01_comm.h
+++ Sources/ESP01_comm.h
@@ -4,4 +4,6 @@
 **         This file exports the needed stuff from ESP01_comm.c
 */
 
-void communication_fsm();
+void comm_fsm_start();
+void comm_fsm_parse(UART2_TComData* message);
+void comm_fsm_send();
