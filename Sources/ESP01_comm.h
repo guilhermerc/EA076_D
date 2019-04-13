09ec8df020d2ae5ec5522360ca54d60d8ff37636
guilhermerc <guilherme.riciolic@gmail.com>
2019-04-13 17:28:41 -0300

    Details in the description
    1. Exporting stuff that will be used outside this file context.
    2. Wrapping global variables into a single global struct.
    3. Renaming the function "comm_start()" to "comm_init()".

--- Sources/ESP01_comm.h
+++ Sources/ESP01_comm.h
@@ -10,14 +10,50 @@
 #include <PE_Types.h>
 #include <UART2.h>
 
+#define TERMINATING_CHARS "\r\n"
 #define MESSAGE_BUFFER_SIZE	512
 
+typedef enum COMM_SENDING_STATUS
+{
+	SENDING,
+	DONE
+} COMM_SENDING_STATUS;
+
+typedef enum COMM_STATUS
+{
+	BUSY,
+	AVAILABLE
+} COMM_STATUS;
+
+typedef enum COMM_STATE
+{
+	CONNECT_WIFI,
+	GET_IP_NUMB,
+	GET_MAC_ADDR,
+	CONNECT_MQTT,
+	WAITING_FOR_CMD,
+	TOPIC_SUBSC,
+	TOPIC_UNSUBSC,
+	PUBLISHING,
+	PINGING
+} COMM_STATE_ENUM;
+
+typedef struct COMM_INFO
+{
+	COMM_STATE_ENUM state;
+	bool message_received;
+	COMM_SENDING_STATUS sending_status;
+	COMM_SENDING_STATUS loging_status;
+
+} COMM_INFO_STRUCT;
+
+COMM_INFO_STRUCT comm_info;
 UART2_TComData message_out[MESSAGE_BUFFER_SIZE];
 UART2_TComData message_in[MESSAGE_BUFFER_SIZE];
 volatile bool message_recv;
-volatile bool message_sent;
 
-void comm_start();
+void comm_init();
+COMM_STATUS comm_status();
 void comm_parse();
 void comm_response();
 
