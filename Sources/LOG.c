6b80f9a4949f013bd0235cda5bf53d2a2b88681b
guilhermerc <guilherme.riciolic@gmail.com>
2019-04-27 00:04:43 -0300

    Adding a feature to the LOG function to just log the string passed as argument
    This is particularly useful to echo the received chars from
    Terminal.

--- Sources/LOG.c
+++ Sources/LOG.c
@@ -21,13 +21,22 @@
 ** 	This handler also notifies the main loop when a
 ** 	log entry is being sent (using the log_entry_sent
 ** 	flags).
+**
+** 	NOTE: If tag is passed as NULL, then the only message logged
+** 	is the 'message' string. This is used to echo the received
+** 	characters from terminal.
 */
 void LOG(char * tag, UART0_TComData * message)
 {
 	comm_info.loging_status = SENDING;
 
-	strcpy(log_buffer, tag);
-	strcat(log_buffer, ": ");
-	strcat(log_buffer, message);
+	if(tag != NULL)
+	{
+		strcpy(log_buffer, tag);
+		strcat(log_buffer, ": ");
+		strcat(log_buffer, message);
+	}
+	else
+		strcpy(log_buffer, message);
 	UART0_OnTxChar();
 }
