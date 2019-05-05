8e958dd66d240055bd474421794081790188bb3c
guilhermerc <guilherme.riciolic@gmail.com>
2019-05-05 01:14:39 -0300

    Uploading minimalist version of display library

---
+++ Sources/display.c
@@ -0,0 +1,17 @@
+/*
+ * display.c
+ *
+ *  Created on: 04/05/2019
+ *      Author: guilherme
+ */
+
+
+void display_init()
+{
+	NOKIA5110_CONTROLLER_Init();
+}
+
+void display_write_line(uint8_t line, char * string)
+{
+	NOKIA5110_CONTROLLER_WriteLineStr(line, string);
+}
---
+++ Sources/display.h
@@ -0,0 +1,14 @@
+/*
+ * display.h
+ *
+ *  Created on: 04/05/2019
+ *      Author: guilherme
+ */
+
+#ifndef SOURCES_DISPLAY_H_
+#define SOURCES_DISPLAY_H_
+
+void display_init();
+void display_write_line(uint8_t line, char * string);
+
+#endif /* SOURCES_DISPLAY_H_ */
